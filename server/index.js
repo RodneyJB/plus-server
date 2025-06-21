ec6152aaf90207c180bd94c409c0fb34fdf7f3e5
Rodney Buljubasic <rodney@bootepolch.local>
2025-06-21 08:43:36 +0200

    add new test

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const replaceParticipantRouter = require('./routes/replaceParticipant');
+const setDateTodayRouter = require('./routes/setDateToday');
+
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 app.use('/replace-participant', replaceParticipantRouter);
+app.use('/set-date', setDateTodayRouter);
+
+
 
 app.get('/', (req, res) => {
   res.send('✅ Plus Server is running');
